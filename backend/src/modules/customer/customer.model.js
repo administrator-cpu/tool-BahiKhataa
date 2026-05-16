389ac7299600368784581e9d4929ae80096322a8
harshjha047 <spyderhj11@gmail.com>
2026-05-16 11:20:12 +0530

    GST

--- backend/src/modules/customer/customer.model.js
+++ backend/src/modules/customer/customer.model.js
@@ -15,7 +15,7 @@ const customerSchema = new mongoose.Schema({
     type: String,
     uppercase: true,
     trim: true,
-    match: [ /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/, "Please enter a valid GST number" ]
+    // match: [ /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/, "Please enter a valid GST number" ]
   },
   email: {
     type: String,
