06c60fe381c6bf30d232a677add8c454bdd1ddc2
Ayush-pathakWD <pathak.ayush2004@gmail.com>
2026-07-09 19:52:32 +0530

    integrate customers for admin in samadhan

--- backend/src/modules/integration/integration.routes.js
+++ backend/src/modules/integration/integration.routes.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import { protectInternalApps } from '../../middlewares/internalAuth.js';
 import {
-  getExternalCustomerFinancials, getExternalFinancialsByName
+  getExternalCustomerFinancials, getExternalFinancialsByName,
+  getAllCustomersFinancials
 } from '../integration/integration.controller.js';
 
 const router = express.Router()
 
 router.use(protectInternalApps);
 
+router.get('/customers/financials/all', getAllCustomersFinancials);
 router.get('/customers/financials/search', getExternalFinancialsByName);
 router.get('/customers/:crmId/financials', getExternalCustomerFinancials);
 
